1df3c3516f5c42d4772d4d99fa3784c3dcd97ada
cjtuey <cjtuey@dmacc.edu>
2025-10-16 23:06:12 -0500

    Added contingency code

--- main.cpp
+++ main.cpp
@@ -30,5 +30,15 @@ void stubs() {
 int main() {
     //    stubs(); // uncomment to run function stubs from each member's source file. Used to ensure the whole project is being integrated properly
     //    utilTest(); // uncomment to run test code for util.h
+
+    // The following is Cian's contingency code, in case no contributions are received by midnight. It's a pale imitation of the project idea, but it's better than nothing.
+    vector<string> drinks = {"Bourbon","Old Fashioned","Rye Whiskey","Espresso Martini","Vodka","Manhattan"};
+    vector<string> recipes = {"Pouring bourbon...","Pouring bourbon...\nAdding sugar...\nAdding Angostura bitters...\nAdding water...\nGarnishing with orange twist...","Pouring rye whiskey...","Pouring vodka...\nAdding coffee liquer...\nAdding espresso...\nAdding simple syrup...\nGarnishing with coffee beans...","Pouring vodka...","Pouring rye whiskey...\nAdding sweet vermouth...\nAdding Angostura bitters...\nGarnishing with brandied cherry..."};
+    vector<double> prices = {4,7,3,8,3,5};
+    int drink = userMenu(&drinks, "Welcome to Club Club! What can I get you? (Enter number):") - 1;
+    if (drink > -1) {
+        cout << recipes.at(drink) << endl;
+        cout << "Done! Your total is $" << prices.at(drink) << endl;
+    }
     return 0;
 }
